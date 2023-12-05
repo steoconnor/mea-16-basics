684131f5ebab6150b3cfd96cbb297ceb99a6c326
steoconnor <steoconnor@hotmail.com>
2023-12-05 10:00:19 +0000

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-echo "I am running" > 1.txt
-echo "Added webhook" > 2.txt
-echo "Extra webhook" > 3.txt
-
-zip archive.zip *.txt 
+echo "Hello from the Jenkins job named: ${JOB_NAME}"
+touch 1.txt 2.txt 3.txt 4.txt 5.txt
+zip archive.zip *.txt
